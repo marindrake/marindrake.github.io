6c22f631e0fa449c2599fa811e501f0f31c3b704
Alex Marin <59639220+marindrake@users.noreply.github.com>
2021-04-23 00:42:52 -0600

    commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+  ]
+
+  var list = document.getElementsByTagName('ol');
+
+  links.forEach(function(link){
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.text = link.label;
+    a.href = link.url;
+    list[0].appendChild(li);
+    li.appendChild(a);
+  });
+
+  console.log(list);
