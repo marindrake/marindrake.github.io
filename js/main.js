4e4c8ad1ad4805640b0001e92654aa2d226ea315
Use your GitHub username <marindrake@gmail.com>
2021-05-06 20:46:45 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const links = [
   { label: 'Week 1 Notes', url: 'week1/index.html', }, // you can add other tags if you like
   { label: 'Week 2 Notes', url: 'week2/index.html', },
+  { label: 'Week 3 Notes', url: 'week3/index.html', },
   ];
 
   var list = document.getElementsByTagName('ol');
