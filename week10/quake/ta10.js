c834ebdc23709a867ea2fad821012b93f7f3852d
Use your GitHub username <marindrake@gmail.com>
2021-06-25 23:54:26 -0600

    update

---
+++ week10/quake/ta10.js
@@ -0,0 +1,16 @@
+
+import { getJSON, getLocation } from './utilities.js';
+import QuakeController from './quakeController.js';
+
+const baseURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+
+const controller = new QuakeController("#quakeList");
+controller.init();
+
+document.querySelector('#radiusButton').addEventListener('click', () => {
+    console.log("value: ", document.querySelector('#radiusInput').value)
+    if(document.querySelector('#radiusInput').value != '') {
+        controller.radius = document.querySelector('#radiusInput').value
+        controller.getQuakesByRadius()
+    }
+})
