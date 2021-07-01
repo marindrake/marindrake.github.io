0af3d3bea7ed4b917a12a092c7ad575ff7582d44
Use your GitHub username <marindrake@gmail.com>
2021-07-01 02:03:06 -0600

    update

--- project/js/main.js
+++ project/js/main.js
@@ -1,22 +1,24 @@
 function onCalculateButtonClick(){
 
-let fname = document.forms["myForm"]["yourname"].value;
-let sname = document.forms["myForm"]["yourcrush"].value;
-const apiURL = "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + fname + "&sname=" + sname
+	let fname = document.forms["myForm"]["yourname"].value;
+	let sname = document.forms["myForm"]["yourcrush"].value;
+	const apiURL = "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + fname + "&sname=" + sname
+		
+	fetch(apiURL, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "287ea89645msh92ef8f80b2c3d85p180b15jsn6b84b278d580",
+			"x-rapidapi-host": "love-calculator.p.rapidapi.com"
+		}
+	})
 	
-fetch(apiURL, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "287ea89645msh92ef8f80b2c3d85p180b15jsn6b84b278d580",
-		"x-rapidapi-host": "love-calculator.p.rapidapi.com"
-	}
-})
-
-.then((response) => response.json())
-.then((jsObject) => {
-  console.log(jsObject);
-  
-  document.getElementById('percentage').textContent = jsObject.percentage;
-  document.getElementById('result').textContent = jsObject.result;
-});
+	.then((response) => response.json())
+	.then((jsObject) => {
+	  console.log(jsObject);
+	  
+	  const resultBlockElement = document.getElementById('main-result-block');
+	  resultBlockElement.classList.remove('invisible');
+	  document.getElementById('percentage').textContent = jsObject.percentage;
+	  document.getElementById('result').textContent = jsObject.result;
+	});
 }
