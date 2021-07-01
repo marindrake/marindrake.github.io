54c27f2ddde7f0adc8d4fc1db2b0e22da8e84464
Use your GitHub username <marindrake@gmail.com>
2021-07-01 01:50:53 -0600

    update

--- project/js/main.js
+++ project/js/main.js
@@ -1,13 +1,22 @@
-fetch("https://love-calculator.p.rapidapi.com/getPercentage?fname=John&sname=Alice", {
+function onCalculateButtonClick(){
+
+let fname = document.forms["myForm"]["yourname"].value;
+let sname = document.forms["myForm"]["yourcrush"].value;
+const apiURL = "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + fname + "&sname=" + sname
+	
+fetch(apiURL, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "287ea89645msh92ef8f80b2c3d85p180b15jsn6b84b278d580",
 		"x-rapidapi-host": "love-calculator.p.rapidapi.com"
 	}
 })
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
+
+.then((response) => response.json())
+.then((jsObject) => {
+  console.log(jsObject);
+  
+  document.getElementById('percentage').textContent = jsObject.percentage;
+  document.getElementById('result').textContent = jsObject.result;
 });
+}
